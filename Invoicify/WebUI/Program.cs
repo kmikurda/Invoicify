97a33ae8b0da05b0f131d4baca00646ddea1f6fa
Kamil Mikurda <kamil.mikurda@interia.pl>
2022-07-26 22:15:51 +0200

    Configure Serilog

--- Invoicify/WebUI/Program.cs
+++ Invoicify/WebUI/Program.cs
@@ -1,13 +1,38 @@
 using Application.Queries;
 using MediatR;
+using Microsoft.Extensions.Logging.Configuration;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMediatR(typeof(GetAllUsersQuery));
 
 
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
+// Serilog config
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+//serilog config end
+
+try
+{
+    Log.Information("asd");
+    Log.Fatal("Fatal {asd}", 10);
+}
+catch (Exception e)
+{
+    Log.Fatal("Fatal error");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
+app.UseSerilogRequestLogging();
 app.Run();

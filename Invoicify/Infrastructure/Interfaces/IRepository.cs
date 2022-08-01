21d023a0478b3536377eb217d88ddec6b54f7bd4
kmikurda@abis.krakow.pl <kmikurda@abis.krakow.pl>
2022-08-01 14:32:48 +0200

    Add base repositories and interfaces

---
+++ Invoicify/Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using Domain.Entities;
+
+namespace Infrastructure.Interfaces;
+
+public interface IRepository
+{
+}
+
+public interface IWriteRepository<T> : IDisposable, IRepository where T : BaseEntity, new()
+{
+    Task<T> GetSingleAsync(int id);
+    Task<T> GetFiltered(Expression<Func<T, bool>> predicate);
+    void Add(T entity);
+    void SoftDelete(T entity);
+    void SoftDeleteWhere(Expression<Func<T, bool>> predicate);
+    Task SaveChangesAsync();
+    Task StartTransaction(CancellationToken token);
+    void CommitTransaction();
+    void RollbackTransaction();
+    Task<List<T>> GetAllAsync();
+
+}
+
+public interface IReadRepository<T> : IDisposable, IRepository where T : BaseEntity, new()
+{
+    Task<List<T>> GetAllAsync();
+    int Count();
+    Task<T> GetSingleAsync(int id);
+    IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+}

de39d793ad7620bbb8c1c46944aad69522d75f35
kmikurda@abis.krakow.pl <kmikurda@abis.krakow.pl>
2022-10-19 07:50:33 +0200

    Add migration with nullable foreign keys

---
+++ Invoicify/Infrastructure/Migrations/20221019053055_SetInvoiceOwnerForeignKeyAsNullable.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class SetInvoiceOwnerForeignKeyAsNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccountingNotes_Contractors_ContractorId",
+                table: "AccountingNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_InterestNotes_Contractors_ContractorId",
+                table: "InterestNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Contractors_ContractorId",
+                table: "Invoices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentDemands_Contractors_ContractorId",
+                table: "PaymentDemands");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "PaymentDemands",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserAssignmentForAuthorizationId",
+                table: "Invoices",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CurrentOwnerId",
+                table: "Invoices",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "Invoices",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "InterestNotes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "AccountingNotes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccountingNotes_Contractors_ContractorId",
+                table: "AccountingNotes",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InterestNotes_Contractors_ContractorId",
+                table: "InterestNotes",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Contractors_ContractorId",
+                table: "Invoices",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentDemands_Contractors_ContractorId",
+                table: "PaymentDemands",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccountingNotes_Contractors_ContractorId",
+                table: "AccountingNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_InterestNotes_Contractors_ContractorId",
+                table: "InterestNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Contractors_ContractorId",
+                table: "Invoices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentDemands_Contractors_ContractorId",
+                table: "PaymentDemands");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "PaymentDemands",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserAssignmentForAuthorizationId",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CurrentOwnerId",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "InterestNotes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractorId",
+                table: "AccountingNotes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccountingNotes_Contractors_ContractorId",
+                table: "AccountingNotes",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InterestNotes_Contractors_ContractorId",
+                table: "InterestNotes",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Contractors_ContractorId",
+                table: "Invoices",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentDemands_Contractors_ContractorId",
+                table: "PaymentDemands",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
